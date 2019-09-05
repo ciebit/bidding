f8b43250bf749e226ddb17b3f88c1a49d824c00c
Johnny Saymon <saymon@ciebit.com.br>
2019-09-05 12:10:43 -0300

    Criacao da classe Organ

---
+++ src/Organ.php
@@ -0,0 +1,29 @@
+<?php
+namespace Ciebit\Bidding;
+
+class Organ
+{
+    /** @var string */
+    private $name;
+
+    /** @var string */
+    private $id;
+
+    public function __construct(
+        string $name,
+        string $id
+    ) {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
