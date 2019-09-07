aae267b7c030003b085ddd663a842a086d44eb89
Johnny Saymon <saymon@ciebit.com.br>
2019-09-07 10:31:55 -0300

    Criacao da classe Publication

---
+++ src/Publication.php
@@ -0,0 +1,71 @@
+<?php
+namespace Ciebit\Bidding;
+
+use DateTime;
+
+class Publication
+{
+    /** @var string */
+    private $biddingId;
+
+    /** @var DateTime */
+    private $date;
+
+    /** @var string */
+    private $description;
+
+    /** @var string */
+    private $fileId;
+
+    /** @var string */
+    private $id;
+
+    /** @var string */
+    private $name;
+
+    public function __construct(
+        string $name,
+        string $description,
+        DateTime $date,
+        string $biddingId,
+        string $fileId,
+        string $id
+    ) {
+        $this->biddingId = $biddingId;
+        $this->date = $date;
+        $this->description = $description;
+        $this->fileId = $fileId;
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    public function getBiddingId(): string
+    {
+        return $this->biddingId;
+    }
+
+    public function getDate(): DateTime
+    {
+        return $this->date;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getFileId(): string
+    {
+        return $this->fileId;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+}
