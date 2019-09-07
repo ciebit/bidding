2aa5dbd4d20c42d03f1a3dee88d0196ca02f9bf6
Johnny Saymon <saymon@ciebit.com.br>
2019-09-07 11:11:54 -0300

    Criacao das classes Contract e Year

---
+++ src/Contract.php
@@ -0,0 +1,140 @@
+<?php
+namespace Ciebit\Bidding;
+
+use DateTime;
+use Ciebit\Bidding\Year;
+
+use function array_push;
+
+class Contract
+{
+    /** @var string */
+    private $biddingId;
+
+    /** @var DateTime */
+    private $date;
+
+    /** @var array */
+    private $filesId;
+
+    /** @var DateTime */
+    private $finalDate;
+
+    /** @var float */
+    private $globalPrice;
+
+    /** @var string */
+    private $id;
+
+    /** @var string */
+    private $number;
+
+    /** @var string */
+    private $objectDescription;
+
+    /** @var string */
+    private $organId;
+
+    /** @var string */
+    private $personId;
+
+    /** @var DateTime */
+    private $startDate;
+
+    /** @var Year */
+    private $yearOfExercise;
+
+
+    public function __construct(
+        string $biddingId,
+        Year $yearOfExercise,
+        string $number,
+        DateTime $date,
+        DateTime $startDate,
+        DateTime $finalDate,
+        float $globalPrice,
+        string $objectDescription,
+        string $organId,
+        string $personId,
+        string $id = ''
+    ) {
+        $this->biddingId = $biddingId;
+        $this->date = $date;
+        $this->filesId = [];
+        $this->finalDate = $finalDate;
+        $this->globalPrice = $globalPrice;
+        $this->id = $id;
+        $this->number = $number;
+        $this->objectDescription = $objectDescription;
+        $this->organId = $organId;
+        $this->personId = $personId;
+        $this->startDate = $startDate;
+        $this->yearOfExercise = $yearOfExercise;
+    }
+
+    public function addFileId(string ...$ids): self
+    {
+        array_push($this->filesId, ...$ids);
+        return $this;
+    }
+
+    public function getBiddingId(): string
+    {
+        return $this->biddingId;
+    }
+
+    public function getDate(): DateTime
+    {
+        return $this->date;
+    }
+
+    public function getFilesId(): array
+    {
+        return $this->filesId;
+    }
+
+    public function getFinalDate(): DateTime
+    {
+        return $this->finalDate;
+    }
+
+    public function getGlobalPrice(): float
+    {
+        return $this->globalPrice;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getNumber(): string
+    {
+        return $this->number;
+    }
+
+    public function getObjectDescription(): string
+    {
+        return $this->objectDescription;
+    }
+
+    public function getOrganId(): string
+    {
+        return $this->organId;
+    }
+
+    public function getPersonId(): string
+    {
+        return $this->personId;
+    }
+
+    public function getStartDate(): DateTime
+    {
+        return $this->startDate;
+    }
+
+    public function getYearOfExercise(): Year
+    {
+        return $this->yearOfExercise;
+    }
+}
---
+++ tests/classes/ContractTest.php
@@ -0,0 +1,89 @@
+<?php
+declare(strict_types=1);
+
+namespace Ciebit\Bidding\Tests;
+
+use Ciebit\Bidding\Contract;
+use Ciebit\Bidding\Year;
+use DateTime;
+use PHPUnit\Framework\TestCase;
+
+class ContractTest extends TestCase
+{
+    /** @var string */
+    private $biddingId = '22';
+
+    /** @var DateTime */
+    private $date;
+
+    /** @var string */
+    private $filesId = ['111','222','333'];
+
+    /** @var DateTime */
+    private $finalDate;
+
+    /** @var float */
+    private $globalPrice = 52682.42;
+
+    /** @var string */
+    private $id = '4444';
+
+    /** @var string */
+    private $number = '5';
+
+    /** @var string */
+    private $objectDescription = 'Object Description';
+
+    /** @var string */
+    private $organId = '55555';
+
+    /** @var string */
+    private $personId = '29';
+
+    /** @var DateTime */
+    private $startDate;
+
+    /** @var Year */
+    private $yearOfExercise;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->date = new DateTime('2019-09-05');
+        $this->startDate = new DateTime('2019-09-06');
+        $this->finalDate = new DateTime('2019-09-07');
+        $this->yearOfExercise = new Year(2019);
+    }
+
+    public function testCreate(): void
+    {
+        $contract = new Contract(
+            $this->biddingId,
+            $this->yearOfExercise,
+            $this->number,
+            $this->date,
+            $this->startDate,
+            $this->finalDate,
+            $this->globalPrice,
+            $this->objectDescription,
+            $this->organId,
+            $this->personId,
+            $this->id
+        );
+        $contract->addFileId(...$this->filesId);
+
+        $this->assertEquals($this->biddingId, $contract->getBiddingId());
+        $this->assertEquals($this->date, $contract->getDate());
+        $this->assertEquals($this->objectDescription, $contract->getObjectDescription());
+        $this->assertEquals($this->filesId, $contract->getFilesId());
+        $this->assertEquals($this->id, $contract->getId());
+        $this->assertEquals($this->finalDate, $contract->getFinalDate());
+        $this->assertEquals($this->globalPrice, $contract->getGlobalPrice());
+        $this->assertEquals($this->number, $contract->getNumber());
+        $this->assertEquals($this->organId, $contract->getOrganId());
+        $this->assertEquals($this->personId, $contract->getPersonId());
+        $this->assertEquals($this->startDate, $contract->getStartDate());
+        $this->assertEquals($this->yearOfExercise, $contract->getYearOfExercise());
+    }
+}
